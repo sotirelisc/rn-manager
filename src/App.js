c80da1fc840e739c2014174b6f55c60d72d4e3b0
sotirelisc <gtakriz@hotmail.com>
2019-03-20 22:27:45 +0200

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {};
+
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
